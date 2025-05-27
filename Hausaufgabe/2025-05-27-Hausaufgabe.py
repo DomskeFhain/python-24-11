c4bc4763e162f73dbcf47aacef48c85427984bd4
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-05-27 18:36:52 +0200

    Create 2025-05-27-Hausaufgabe.py

---
+++ Hausaufgabe/2025-05-27-Hausaufgabe.py
@@ -0,0 +1,14 @@
+## Aufgabe 1:
+forV = 7 ## for ist schon ein definierter Befehl weswegen er keine variable sein kann
+print(forV) 
+
+## Aufgabe 2:
+name = "Ada"
+if name == "Ada":
+    print("Hello,", name) ## Print musste eingerückt werden, da die Zeile sonst nicht ausgegeben wird wegen dem "if"
+
+## Aufgabe 3:
+price  = 19.99   # als String (den fehler hast du hier schon geschrieben dass es als String angegeben wird.
+quantity = 3       # dadurch ist Total als String weiterhin und wiederholt nur die Zahl)
+total = price * quantity
+print("Total:", total) ## 19.9919.99.19.99
