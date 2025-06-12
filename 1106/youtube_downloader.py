acf49ed7c0650c322a319c5f60d28deddfcaca71
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-06-12 11:40:02 +0200

    Update youtube_downloader.py

--- 1106/youtube_downloader.py
+++ 1106/youtube_downloader.py
@@ -6,12 +6,12 @@ youtube_format = input("Welches Format möchtest du haben ? (audio/video): ")
 
 if youtube_format.lower() == "video":
     print("Video wird heruntergeladen..")
-    YouTube(url).streams.get_highest_resolution().download("/Users/suheibmarzouka/Desktop/24-01-ON/Workspaces/python-24-11/1106/Youtube Videos")
+    YouTube(url).streams.get_highest_resolution().download("H:\Youtube+")
     print("Video wurde erfolgreich heruntergeladen")
 
 elif youtube_format.lower() == "audio":
     print("Audio wird heruntergeladen..")
-    YouTube(url).streams.get_audio_only().download("/Users/suheibmarzouka/Desktop/24-01-ON/Workspaces/python-24-11/1106/Youtube Audios")
+    YouTube(url).streams.get_audio_only().download("G:\Youtube+")
     print("Audio wurde erfolgreich heruntergeladen")
 
 else:
